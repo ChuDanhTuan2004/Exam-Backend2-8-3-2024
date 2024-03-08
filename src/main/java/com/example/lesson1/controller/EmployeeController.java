f514d74579e2a75a2c11270e105fea3775e09601
chudanhtuan <chudanhtuan2034@gmail.com>
2024-03-08 16:06:32 +0700

    done

--- src/main/java/com/example/lesson1/controller/EmployeeController.java
+++ src/main/java/com/example/lesson1/controller/EmployeeController.java
@@ -1,21 +1,17 @@
 package com.example.lesson1.controller;
 
 import com.example.lesson1.model.Employee;
-import com.example.lesson1.model.PageWeb;
 import com.example.lesson1.repository.IEmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.awt.print.Pageable;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @RestController
@@ -24,20 +20,13 @@ public class EmployeeController {
     @Autowired
     IEmployeeRepository employeeRepository;
 
-//    @GetMapping("")
-//    public ResponseEntity findAll(@RequestBody PageWeb pageWeb) {
-//        Pageable pageable = PageRequest.of(pageWeb.getPage(), pageWeb.getSize(), Sort.by("id"));
-//        Page<Employee> employees = employeeRepository.findAll(pageable);
-//        List<Employee> employeeList = new ArrayList<>();
-//        for ( Employee employee : employees){
-//            employeeList.add(employee);
-//        }
-//        return new ResponseEntity(employeeList, HttpStatus.OK);
-//    }
-
     @GetMapping("")
-    public ResponseEntity findAll() {
-        return new ResponseEntity(employeeRepository.findAll(), HttpStatus.OK);
+    public ResponseEntity findAll(@PageableDefault(value = 5) Pageable pageable) {
+        Page<Employee> employees = employeeRepository.findAll(pageable);
+        if (employees.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity(employees, HttpStatus.OK);
     }
 
 
@@ -67,17 +56,17 @@ public class EmployeeController {
     }
 
     @GetMapping("/findName")
-    public ResponseEntity findName(@RequestParam String name) {
-        return new ResponseEntity(employeeRepository.findAllByNameContaining(name), HttpStatus.OK);
+    public ResponseEntity findName(@RequestParam String name, @PageableDefault(value = 5) Pageable pageable) {
+        return new ResponseEntity((Page<Employee>) employeeRepository.findAllByNameContaining(name), HttpStatus.OK);
     }
 
     @GetMapping("/findAddress")
-    public ResponseEntity findAddress(@RequestParam String address) {
-        return new ResponseEntity(employeeRepository.findAllByAddressContaining(address), HttpStatus.OK);
+    public ResponseEntity findAddress(@RequestParam String address, @PageableDefault(value = 5) Pageable pageable) {
+        return new ResponseEntity((Page<Employee>) employeeRepository.findAllByAddressContaining(address), HttpStatus.OK);
     }
 
     @GetMapping("/findAge")
-    public ResponseEntity findAge(@RequestParam int age) {
-        return new ResponseEntity(employeeRepository.findAllByAgeContaining(age), HttpStatus.OK);
+    public ResponseEntity findAge(@RequestParam int age, @PageableDefault(value = 5) Pageable pageable) {
+        return new ResponseEntity((Page<Employee>) employeeRepository.findAllByAgeContaining(age), HttpStatus.OK);
     }
 }
